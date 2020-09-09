ef5f835f4b41fbf8c0b16e07b6594dec57df96e6
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2020-09-09 12:12:00 +0700

    :zap: add link prefetch to improve performance

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+function prefetch(e) {
+  if (e.target.tagName != "A") {
+    return;
+  }
+  if (e.target.origin != location.origin) {
+    return;
+  }
+  var l = document.createElement("link");
+  l.rel = "prefetch";
+  l.href = e.target.href;
+  document.head.appendChild(l);
+}
+document.documentElement.addEventListener("mouseover", prefetch, {
+  capture: true,
+  passive: true,
+});
+document.documentElement.addEventListener("touchstart", prefetch, {
+  capture: true,
+  passive: true,
+});
